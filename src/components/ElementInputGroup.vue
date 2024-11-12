541fe6b038ccbf5d93d5bb1b39236815aae035a9
jxtou <jxtou@xirelogy.com>
2024-11-12 21:21:00 +0800

    Adapt VanyInputGroup to VanyPopup/VanyDropdownMenu

--- src/components/ElementInputGroup.vue
+++ src/components/ElementInputGroup.vue
@@ -8,6 +8,10 @@ import {
 } from 'vue';
 
 import {
+  _cast,
+} from '@xirelogy/xwts';
+
+import {
   type MinVNode,
   VanyRenderAttributes,
   VanyRenderer,
@@ -20,6 +24,30 @@ const attrs = new VanyRenderAttributes({
 
 const slots = useSlots();
 
+
+/**
+ * Try to get specific slot component via current component
+ * @param target
+ * @param slotName
+ * @returns
+ */
+function getComponentVNodeViaSlot(target: MinVNode, slotName: string): MinVNode {
+  const _target = _cast<VNode>(target);
+  if (!_target.children) return target; // Blocked
+
+  const _children = _target.children;
+  if (typeof _children !== 'object') return target; // Blocked
+
+  const child = _cast<Record<string, any>>(_children)[slotName];
+  if (typeof child !== 'function') return target; // Blocked
+
+  const res = child();
+  if (!Array.isArray(res)) return target; // Blocked
+
+  return res[0];
+}
+
+
 const render = () => {
   attrs.mergeAttrsClass([
     'el-input',
@@ -30,21 +58,31 @@ const render = () => {
   const slotContents = VanyVue.patchVNodes(
     VanyRenderer.acceptSlot(slots.default)(),
     (target: MinVNode) => {
+      let refTarget = target;
+
       // Text node shall be wrapped in span
-      if (VanyVue.isTextVNode(target)) {
+      if (VanyVue.isTextVNode(refTarget)) {
         return h('span', {
           class: 'el-vany-input-group__item',
         }, [ target as VNode ]);
       }
 
+      // First pass
+      const firstTargetCompType = VanyVue.getComponentVNodeType(refTarget);
+      switch (firstTargetCompType) {
+        case 'VanyDropdownMenu':
+        case 'VanyPopup':
+        refTarget = getComponentVNodeViaSlot(refTarget, 'reference');
+      }
+
       // Native nodes are being cloned with additional class
-      if (typeof target.type === 'string' && target.type != 'input') {
+      if (typeof refTarget.type === 'string' && refTarget.type != 'input') {
         return cloneVNode(target as VNode, {
           class: 'el-vany-input-group__item',
         });
       }
 
-      const targetCompType = VanyVue.getComponentVNodeType(target);
+      const targetCompType = VanyVue.getComponentVNodeType(refTarget);
       if (targetCompType === null) return target;
 
       switch (targetCompType) {
